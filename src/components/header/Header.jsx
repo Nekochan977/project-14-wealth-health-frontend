13ba11c7c8e85c8c80c8be244ac4ca5b28d56e31
Nekochan977 <nadja.talaouitmockli@yahoo.com>
2023-05-16 10:45:34 +0200

    header component

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,10 @@
+import Logo from "../../assets/Logo.png"
+
+const Header = () => {
+    return(
+        <header>
+            <img src={Logo} alt="HRnet logo"/>
+        </header>
+    )
+}
+export default Header
