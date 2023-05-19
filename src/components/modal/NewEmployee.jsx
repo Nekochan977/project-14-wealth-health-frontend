d9125cd96ae63408e8dc8a898b26983f4b93a615
Nekochan977 <nadja.talaouitmockli@yahoo.com>
2023-05-19 11:10:08 +0200

    base for form modal

---
+++ src/components/modal/NewEmployee.jsx
@@ -0,0 +1,14 @@
+
+import "./NewEmployee.css"
+
+const NewEmployee = () => {
+    return(
+        <form className="form">
+            <label>First Name</label>
+            <input name="first-name" id="first-name" type="text"/>
+
+
+        </form>
+    )
+}
+export default NewEmployee
