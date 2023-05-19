f4ea23e89d33f2e8e5856c8b190a24c04efb48e8
Nekochan977 <nadja.talaouitmockli@yahoo.com>
2023-05-19 11:16:34 +0200

    preparation of functions and states to show form modal

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import './App.css';
 import Header from "./components/header/Header";
 import Button from "./components/button/Button";
+import {useState} from "react";
+import NewEmployee from "./components/modal/NewEmployee";
+
 
 function App() {
+    const [isShown, setIsShown] = useState(true)
+    const [isOpen, setIsOpen] = useState(false);
+
+    function toggle() {
+        setIsOpen((isOpen) => !isOpen);
+        // setIsShown((isShown) => !isShown)
+    }
+
+
+
   return (
       <div className="App">
           <Header/>
           <main className="main-container">
-              <div className="button-container"></div>
-              <Button text={"Create new employee"}/>
-              <Button text={"View current employees"} />
+              {isShown &&
+                  <div className="button-container">
+                      <Button text={"Create new employee"} click={toggle}/>
+                      <Button text={"View current employees"}/>
+                  </div>
+              }
+              {isOpen && <NewEmployee/>}
           </main>
       </div>
   );
