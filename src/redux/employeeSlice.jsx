af83ba430edb4727f3ac0391dc11db4441fa91ea
Nekochan977 <nadja.talaouitmockli@yahoo.com>
2023-07-21 14:47:44 +0200

    clean

--- src/redux/employeeSlice.jsx
+++ src/redux/employeeSlice.jsx
@@ -1,17 +1,5 @@
 import {createSlice} from "@reduxjs/toolkit";
-import {number} from "prop-types";
 
-// const initialState = [{
-//     firstName: "",
-//     lastName: "",
-//     startDate: "",
-//     department: "",
-//     birthDate: "",
-//     street: "",
-//     city: "",
-//     state: "",
-//     zipCode: number
-// }]
 const initialState = []
 
 const employeesSlice = createSlice({
