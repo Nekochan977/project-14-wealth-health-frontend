14284186f8d6ab6fee6570dd9e3288584d51b673
Nekochan977 <nadja.talaouitmockli@yahoo.com>
2023-07-25 16:50:47 +0200

    link home

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,10 +1,15 @@
 import Table from "../components/table/Table";
+import {HOME} from "../Routes";
+import {Link} from "react-router-dom";
 
 const Employees = () => {
     return (
         <main className={"container mx-auto"}>
             {/*<h2 className={"text-center my-4 text-xl text-violet-700"}>Employee list</h2>*/}
             <Table/>
+            <Link to={HOME}>
+                <span className={"logo homeLink"}>Home</span>
+            </Link>
         </main>
     )
 }
