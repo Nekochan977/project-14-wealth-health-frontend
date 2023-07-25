23ec722f294f2cc175bb97f130068f5bbcb1be40
Nekochan977 <nadja.talaouitmockli@yahoo.com>
2023-07-25 16:51:41 +0200

    clean

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -5,7 +5,6 @@ import {Link} from "react-router-dom";
 const Employees = () => {
     return (
         <main className={"container mx-auto"}>
-            {/*<h2 className={"text-center my-4 text-xl text-violet-700"}>Employee list</h2>*/}
             <Table/>
             <Link to={HOME}>
                 <span className={"logo homeLink"}>Home</span>
