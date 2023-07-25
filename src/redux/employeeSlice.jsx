53d0a3003fe14c94484e2142cdb54653d27e1e33
Nekochan977 <nadja.talaouitmockli@yahoo.com>
2023-07-25 15:11:54 +0200

    add 2 employees data to initial state

--- src/redux/employeeSlice.jsx
+++ src/redux/employeeSlice.jsx
@@ -1,6 +1,33 @@
 import {createSlice} from "@reduxjs/toolkit";
 
-const initialState = []
+const initialState = [
+    {
+        firstName: "Maya",
+        lastName:"Labeille",
+        socialNumber: "87647653674370987",
+        birthDate: "January 25th, 1984",
+        startDate: "July 3rd, 2023",
+        streetName: "1 sunflower",
+        city: "Petal",
+        zipCode: "12345",
+        state: {name: "California", abbreviation: "CA"},
+        contractType: "Full-Time",
+        department: "sales"
+    },
+    {
+        firstName: "Thomas",
+        lastName:"Coulet",
+        socialNumber: "0868657674370654",
+        birthDate: "February 10th, 1990",
+        startDate: "July 23rd, 2023",
+        streetName: "5 Yellow Star",
+        city: "Moon",
+        zipCode: "87654",
+        state: {name: "Alabama", abbreviation: "AL"},
+        contractType: "Part-Time",
+        department: "marketing"
+    },
+]
 
 const employeesSlice = createSlice({
     name: 'employees',
